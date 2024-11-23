ce965ba5e957f6de546b59470eb6e1d978ea9aa6
Bennet Strauch <116448293+bennetstrauch@users.noreply.github.com>
2024-11-22 21:23:27 -0600

    small last change :)

--- backend/src/main/java/spring/backend/security/SecurityConfig.java
+++ backend/src/main/java/spring/backend/security/SecurityConfig.java
@@ -82,6 +82,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        // change to your frontend port:
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
